3d4213f517479c2563a7ead67f07f86c56d12060
jjduverge <joelduverge321@gmail.com>
2022-05-10 15:05:57 -0500

    updated name of role

--- iam.tf
+++ iam.tf
@@ -37,17 +37,17 @@ data "aws_iam_policy_document" "assume" {
   }
 }
 
-resource "aws_iam_role" "awsconfig" {
+resource "aws_iam_role" "this" {
   name_prefix        = "aws-config-role"
   assume_role_policy = data.aws_iam_policy_document.assume.json
 }
 
 resource "aws_iam_role_policy_attachment" "awsconfig_managed_policy" {
-  role       = aws_iam_role.awsconfig.name
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
 }
 
 resource "aws_iam_role_policy_attachment" "awsconfig_local_policy" {
-  role       = aws_iam_role.awsconfig.name
+  role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.awsconfig.arn
 }
